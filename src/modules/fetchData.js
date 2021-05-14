02a3be563f5a209b4dc1b7031fe6b1c10fcc25f8
rohit.chakraborty <rohchakr@gmail.com>
2021-05-14 11:40:28 +0530

    Fix build failure

--- src/modules/fetchData.js
+++ src/modules/fetchData.js
@@ -1,20 +1,20 @@
-const RC_MARVEL_PUBLIC_KEY = '';
-const EP_MARVEL_BASE = '';
-const EP_ALL_CHARACTERS = '';
+// const RC_MARVEL_PUBLIC_KEY = '';
+// const EP_MARVEL_BASE = '';
+// const EP_ALL_CHARACTERS = '';
 
-const fetchAllCharacters = () => {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            return xhr.response;
-        }
-    }
-    const method = 'GET';
-    const url = EP_MARVEL_BASE + EP_ALL_CHARACTERS + '?apikey=' + RC_MARVEL_PUBLIC_KEY;
-    const async = true;
-    xhr.open(method, url, async);
-    xhr.send();
-}
+// const fetchAllCharacters = () => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function () {
+//         if (this.readyState === 4) {
+//             return xhr.response;
+//         }
+//     }
+//     const method = 'GET';
+//     const url = EP_MARVEL_BASE + EP_ALL_CHARACTERS + '?apikey=' + RC_MARVEL_PUBLIC_KEY;
+//     const async = true;
+//     xhr.open(method, url, async);
+//     xhr.send();
+// }
 
 const fetchData = () => {
     // return fetchAllCharacters();
